3184dfa74e2e4ed633ef6cbfd9c53d2e12eaab17
romnnn <roman.dahm@gmx.de>
2021-02-03 00:22:20 +0100

    fix stable repo url

--- deployment/helm/charts/ldapmanager/add-repos.sh
+++ deployment/helm/charts/ldapmanager/add-repos.sh
@@ -4,7 +4,7 @@ set -e
 export DIR=$(dirname $0)
 
 # openldap chart is in the @stable repository
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add stable https://charts.helm.sh/stable
 
 echo "Updating dependencies for $(realpath $DIR)..."
 helm dependency update $(realpath $DIR)
