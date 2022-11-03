e0a9960d7c76ad7b028dbebf2a76d4b72ba0f30b
romnnn <roman.dahm@gmx.de>
2022-11-03 15:18:10 +0100

    add tests

---
+++ pkg/get_user_groups_test.go
@@ -0,0 +1,74 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/romnn/go-recursive-sort"
+	pb "github.com/romnn/ldap-manager/pkg/grpc/gen"
+)
+
+// TestGetUserGroups tests getting the groups a user is member of
+func TestGetUserGroups(t *testing.T) {
+	test := new(Test).Setup(t)
+	defer test.Teardown()
+
+	username := "test-user"
+	req := pb.NewUserRequest{
+		Username:  username,
+		Password:  "Hallo Welt",
+		Email:     "a@b.de",
+		FirstName: "roman",
+		LastName:  "d",
+	}
+
+	// create a new user (will create the user group)
+	if err := test.Manager.NewUser(&req); err != nil {
+		t.Fatalf("failed to add new user: %v", err)
+	}
+
+	// create a new group
+	strict := false
+	groupName := "test-group"
+	if err := test.Manager.NewGroup(&pb.NewGroupRequest{
+		Name:    groupName,
+		Members: []string{username},
+	}, strict); err != nil {
+		t.Fatalf("failed to add new group: %v", err)
+	}
+
+	expected := &pb.GroupList{
+		Groups: []*pb.Group{
+			{
+				Name: groupName,
+				Members: []string{
+					test.Manager.UserNamed(username),
+				},
+				GID: 2002,
+			},
+			{
+				Name: "users",
+				Members: []string{
+					"uid=ldapadmin,ou=users,dc=example,dc=org",
+					test.Manager.UserNamed(username),
+				},
+				GID: 2001,
+			},
+		},
+		Total: 2,
+	}
+
+	groups, err := test.Manager.GetUserGroups(&pb.GetUserGroupsRequest{
+		Username: username,
+	})
+	if err != nil {
+		t.Fatalf("failed to get the groups of user %q: %v", username, err)
+	}
+
+	sort := recursivesort.RecursiveSort{StructSortField: "GID"}
+	sort.Sort(&groups)
+	sort.Sort(&expected)
+
+	if equal, diff := EqualProto(expected, groups); !equal {
+		t.Fatalf("unexpected groups: \n%s", diff)
+	}
+}
---
+++ pkg/get_user_list_test.go
@@ -0,0 +1,74 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/romnn/go-recursive-sort"
+	pb "github.com/romnn/ldap-manager/pkg/grpc/gen"
+)
+
+// TestGetUserList tests getting a list of all users
+func TestGetUserList(t *testing.T) {
+	test := new(Test).Setup(t)
+	defer test.Teardown()
+
+	req := pb.NewUserRequest{
+		Username:  "test-user",
+		Password:  "Hallo Welt",
+		Email:     "a@b.de",
+		FirstName: "roman",
+		LastName:  "d",
+	}
+
+	// create a new user (will create the user group)
+	if err := test.Manager.NewUser(&req); err != nil {
+		t.Fatalf("failed to add new user: %v", err)
+	}
+
+	// get all users
+	users, err := test.Manager.GetUserList(&pb.GetUserListRequest{})
+	if err != nil {
+		t.Fatalf("failed to get list of users: %v", err)
+	}
+
+	expected := &pb.UserList{
+		Users: []*pb.User{
+			{
+				Username:      req.GetUsername(),
+				FirstName:     req.GetFirstName(),
+				LastName:      req.GetLastName(),
+				DisplayName:   "roman d",
+				CN:            "roman d",
+				Email:         req.GetEmail(),
+				UID:           2001,
+				GID:           2001,
+				LoginShell:    "/bin/bash",
+				HomeDirectory: "/home/test-user",
+			},
+			{
+				Username:      test.Manager.DefaultAdminUsername,
+				FirstName:     "changeme",
+				LastName:      "changeme",
+				DisplayName:   "changeme changeme",
+				CN:            "changeme changeme",
+				Email:         "changeme@changeme.com",
+				UID:           2000,
+				GID:           2001,
+				LoginShell:    "/bin/bash",
+				HomeDirectory: "/home/ldapadmin",
+			},
+		},
+		Total: 2,
+	}
+
+	sort := recursivesort.RecursiveSort{StructSortField: "GID"}
+	sort.Sort(&users)
+	sort.Sort(&expected)
+
+	t.Log(PrettyPrint(users))
+	t.Log(PrettyPrint(expected))
+
+	if equal, diff := EqualProto(expected, users); !equal {
+		t.Fatalf("unexpected users: \n%s", diff)
+	}
+}
